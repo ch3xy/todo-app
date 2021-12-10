e2e028d747c743f59046d9918f92e1a910b8d642
Markus Krakora <markus.krakora@extern.brz.gv.at>
2021-12-10 11:10:40 +0100

    test commit

--- src/main/java/com/ebcont/todoapp/TodoAppApplication.java
+++ src/main/java/com/ebcont/todoapp/TodoAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoAppApplication {
-
+	// Bitte lösch mich
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
