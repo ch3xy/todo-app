93f78d5a8744c68c8ace9d52c6001b3dfff9e419
igor.strilic <igor.strilic@ebcont.com>
2021-12-10 13:34:15 +0100

    added first end to end test

--- src/test/java/com/ebcont/todoapp/TodoAppApplicationTests.java
+++ src/test/java/com/ebcont/todoapp/TodoAppApplicationTests.java
@@ -2,12 +2,22 @@ package com.ebcont.todoapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest
 class TodoAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test_listAllTodos() {
+		WebTestClient
+				.bindToServer()
+				.baseUrl("http://localhost:8080")
+				.build()
+				.get()
+				.uri("/api/todos")
+				.exchange()
+				.expectStatus().isOk()
+//				.expectHeader().valueEquals("Content-Type", "application/json")
+				.expectBody().json("[\"hello world\"]");
 	}
-
 }
